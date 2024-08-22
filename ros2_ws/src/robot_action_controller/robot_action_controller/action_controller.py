fcd11bb931e794d3626003eb58aa0a51da7b9277
robin-karlsson0 <robin.karlsson0@gmail.com>
2024-08-22 15:01:54 +0300

    ADD Callback chain action cycle

---
+++ ros2_ws/src/robot_action_controller/robot_action_controller/action_controller.py
@@ -0,0 +1,117 @@
+import rclpy
+from rclpy.node import Node
+from robot_state_interfaces.srv import State
+from std_msgs.msg import String
+
+
+class ActionController(Node):
+    '''
+    '''
+    def __init__(self):
+        super().__init__('action_controller')
+
+        ##################
+        #  State client
+        ##################
+        self.state_cli = self.create_client(State, 'get_state')
+        self.action_cli = self.create_client(State, 'get_action')
+
+        while not self.state_cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+
+        self.state_req = State.Request()
+        self.action_req = State.Request()
+
+        # Action response publisher
+        self.action_resp_pub = self.create_publisher(String, 'action_responese', 10)
+
+        # Timer for action cycle
+        self.timer = self.create_timer(0.1, self.run_action_cycle)
+
+    def run_action_cycle(self):
+        '''
+        Runs an action cycle represented by a sequence of callback functions.
+
+        1. Get current state
+        --> 2. Get action decision
+            --> 3. Execute action
+        '''
+        self.get_current_state()
+
+    def get_current_state(self):
+        '''
+        '''
+        state_future = self.state_cli.call_async(self.state_req)
+        state_future.add_done_callback(self.get_action)
+
+    def get_action(self, state_future):
+        '''
+        '''
+        try:
+            state_resp = state_future.result()
+            state = state_resp.out_state
+            self.get_logger().info('Received state')
+
+        except Exception as e:
+            self.get_logger().info('Service call failed %r' % (e,))
+            return None
+
+        self.action_req.in_state = state
+        action_future = self.action_cli.call_async(self.action_req)
+        action_future.add_done_callback(self.execute_action)
+
+    def execute_action(self, action_future):
+        '''
+        '''
+        try:
+            action_resp = action_future.result()
+            action = action_resp.out_state
+            self.get_logger().info('Received action')
+
+        except Exception as e:
+            self.get_logger().info('Service call failed %r' % (e,))
+            return None
+
+        # TODO How to handle action execution
+        # 1. Select appropriate action wrapper
+        # 2. Action wrapper executes action
+        # 3. Action wrapper returns action response
+        # 4. Action response is published to /action_response
+        # 5. Callback chain terminates
+
+        if action == 'a':
+            # TODO Do nothing action
+            self.get_logger().info('Executing action a')
+            action_resp = "No action"
+
+        elif action == 'b':
+            self.get_logger().info('Executing action b')
+
+        else:
+            self.get_logger().info(f'Invalid action received: {action}')
+
+        msg = String()
+        msg.data = action_resp
+        self.action_resp_pub.publish(msg)
+
+        # Execute action
+        # if action == 'action_1':
+        #     self.get_logger().info('Executing action 1')
+        # elif action == 'action_2':
+        #     self.get_logger().info('Executing action 2')
+        # else:
+        #     self.get_logger().info('No action to execute')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    state_client = ActionController()
+    state_client.get_logger().info('Action controller is running...')
+    rclpy.spin(state_client)
+
+    state_client.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
