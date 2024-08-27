4b2a4703b27b6dede4b73962f13d0ff39010dd37
Robin Karlsson <robin.karlsson0@gmail.com>
2024-08-27 13:06:33 +0300

    ADD Refactored and working ActionDecision node

---
+++ ros2_ws/src/robot_action_controller/robot_action_controller/action_decision.py
@@ -0,0 +1,90 @@
+import asyncio
+
+import rclpy
+from llm_action_interfaces.action import LLM
+from rclpy.action import ActionClient, ActionServer
+from rclpy.node import Node
+from robot_action_interfaces.action import ActionDecision
+
+
+class ActionDecisionActionServer(Node):
+    '''
+    '''
+
+    def __init__(self):
+        '''
+        '''
+        super().__init__('action_decision')
+
+        self._action_server = ActionServer(
+            self,
+            ActionDecision,
+            'pred_action',
+            execute_callback=self.execute_callback,
+        )
+
+        self._action_client = ActionClient(self, LLM, 'llm_action_server')
+
+        # All valid actions represented by a single token output
+        self.max_tokens = 1
+        self.do_nothing_action = 'a'
+
+    async def execute_callback(self, goal_handle):
+        '''
+        Callback function that sends the received goal to the LLM action server
+        and returns the response as an LLM.Result() message.
+        '''
+        # Unpack ActionDecision.Goal() msg
+        goal = goal_handle.request
+        state = goal.state
+
+        # Create action decision prompt
+        prompt = 'Task: Predict the optimal action to take in order to achieve the goal based on the following robot world state:\n\n'
+        prompt += state
+        prompt += '\n\nOutput the alphabet character of the optimal action choice in the following list of actions:\n\n'
+        prompt += f'Do nothing: {self.do_nothing_action}\n'
+        prompt += 'Formulate a vocal reply: b\n'
+        prompt += 'Character of optimal action choice: '
+
+        llm_goal = LLM.Goal()
+        llm_goal.prompt = prompt
+        llm_goal.max_tokens = self.max_tokens
+
+        # (1) Send goal asynchronously
+        send_goal_future = await self._action_client.send_goal_async(llm_goal)
+
+        # Return "do nothing" action if goal is not accepted
+        if not send_goal_future.accepted:
+            goal_handle.abort()
+            result_msg = ActionDecision.Result()
+            result_msg.pred_action = self.do_nothing_action
+            return result_msg
+
+        # (2) Wait for the result asynchronously
+        llm_result = await send_goal_future.get_result_async()
+
+        # Unpack LLM.Result() msg
+        llm_result_msg = llm_result.result
+
+        result = ActionDecision.Result()
+        result.pred_action = llm_result_msg.response
+
+        goal_handle.succeed()
+        self.get_logger().info(f'Return response: {result}')
+
+        return result
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = ActionDecisionActionServer()
+
+    asyncio.run(rclpy.spin(node))
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- ros2_ws/src/robot_action_controller/setup.py
+++ ros2_ws/src/robot_action_controller/setup.py
@@ -8,10 +8,14 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'llm_action_interfaces',
+        'robot_action_interfaces',
+    ],
     zip_safe=True,
     maintainer='robin',
     maintainer_email='robin.karlsson0@gmail.com',
@@ -20,6 +24,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'action_controller = ' + package_name + '.action_controller:main',
+            'action_decision = ' + package_name + '.action_decision:main',
         ],
     },
 )
