634bd455852389a6a27c11983584e643351be363
Robin Karlsson <robin.karlsson0@gmail.com>
2025-05-27 23:30:00 +0900

    ADD State manager

---
+++ ros2_ws/src/state_manager/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'state_manager'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools',
+        'exodapt_robot_interfaces',
+    ],
+    zip_safe=True,
+    maintainer='robin',
+    maintainer_email='robin.karlsson0@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'state_manager = ' + package_name + '.state_manager:main',
+        ],
+    },
+)
