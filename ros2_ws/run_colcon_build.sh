f5d2e0b56a81eae23c802247674cac3c89a6ebb7
Robin Karlsson <robin.karlsson0@gmail.com>
2025-05-28 22:47:07 +0900

    FIX PYTHONPATH location

--- ros2_ws/run_colcon_build.sh
+++ ros2_ws/run_colcon_build.sh
@@ -2,9 +2,9 @@
 source ../decidapt_env/bin/activate
 source /opt/ros/jazzy/setup.bash
 
-# Ensure colcon uses the virtual environment's Python
-export PYTHONPATH="${VIRTUAL_ENV}/lib/python3.12/site-packages:${PYTHONPATH}"
-
 colcon build --symlink-install
 
-source install/setup.bash 
+source install/setup.bash
+
+# Ensure ROS 2 packages uses the virtual environment's Python
+export PYTHONPATH="${VIRTUAL_ENV}/lib/python3.12/site-packages:${PYTHONPATH}"
