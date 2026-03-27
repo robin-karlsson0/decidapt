02b160562a5b5e6d1ddc58673e958cdca0724b71
Robin Karlsson <robin.karlsson0@gmail.com>
2026-03-27 09:30:27 +0900

    ADD Launch file

--- ros2_ws/src/async_state_recon/setup.py
+++ ros2_ws/src/async_state_recon/setup.py
@@ -1,15 +1,20 @@
+import glob
+import os
+
 from setuptools import find_packages, setup
 
 package_name = 'async_state_recon'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
     ],
     install_requires=['setuptools', 'exodapt_robot_interfaces'],
     zip_safe=True,
